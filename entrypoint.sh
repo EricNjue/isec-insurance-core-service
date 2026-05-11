b707ea126822047449749ca0accd5ef447c135ea
Eric Gitonga Njue <eric.gitonga38@gmail.com>
2026-05-11 23:48:57 +0300

    Optimize Liquibase integration in Docker: install CLI for faster migrations, simplify changelog setup, and streamline `entrypoint.sh`.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 # Run Liquibase Migrations
 echo "Running database migrations..."
 # Update properties with environment variables if they exist
-if [ -n "$DB_URL" ]; then sed -i "s|url=.*|url=$DB_URL|g" app-bootstrap/liquibase.properties || sed -i "" "s|url=.*|url=$DB_URL|g" app-bootstrap/liquibase.properties; fi
-if [ -n "$DB_USERNAME" ]; then sed -i "s|username=.*|username=$DB_USERNAME|g" app-bootstrap/liquibase.properties || sed -i "" "s|username=.*|username=$DB_USERNAME|g" app-bootstrap/liquibase.properties; fi
-if [ -n "$DB_PASSWORD" ]; then sed -i "s|password=.*|password=$DB_PASSWORD|g" app-bootstrap/liquibase.properties || sed -i "" "s|password=.*|password=$DB_PASSWORD|g" app-bootstrap/liquibase.properties; fi
+if [ -n "$DB_URL" ]; then sed -i "s|url=.*|url=$DB_URL|g" liquibase.properties || sed -i "" "s|url=.*|url=$DB_URL|g" liquibase.properties; fi
+if [ -n "$DB_USERNAME" ]; then sed -i "s|username=.*|username=$DB_USERNAME|g" liquibase.properties || sed -i "" "s|username=.*|username=$DB_USERNAME|g" liquibase.properties; fi
+if [ -n "$DB_PASSWORD" ]; then sed -i "s|password=.*|password=$DB_PASSWORD|g" liquibase.properties || sed -i "" "s|password=.*|password=$DB_PASSWORD|g" liquibase.properties; fi
 
-./mvnw liquibase:update -pl app-bootstrap -am -Dliquibase.propertyFile=app-bootstrap/liquibase.properties
+liquibase --defaults-file=liquibase.properties update
 
 # Start the Application
 echo "Starting the application..."
